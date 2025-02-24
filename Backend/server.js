ef38670dfe719a871be783e87a331a4496838f0e
Harshit <harshit_work@outlook.com>
2025-02-24 23:18:21 +0530

    final commit to submit for byterush

--- Backend/server.js
+++ Backend/server.js
@@ -1,23 +1,19 @@
-import express, { json } from 'express';
-import cors from 'cors';
-import { GoogleGenerativeAI } from "@google/generative-ai";
-require('dotenv').config()
-
-console.log(process.env.API);
+const express = require('express');
+const cors = require('cors');
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+require('dotenv').config();
 
 const genAI = new GoogleGenerativeAI(process.env.API);
-const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" })
+const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
-const app = express()
-const port = 3000
-app.use(cors())
-app.use(json())
-
-console.log(process.env)
+const app = express();
+const port = 3000;
+app.use(cors());
+app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.send("Hello World")
-})
+    res.send("Hello World");
+});
 
 app.post('/chat', async (req, res) => {
     try {
@@ -34,14 +30,13 @@ app.post('/chat', async (req, res) => {
         const result = await chat.sendMessage(msg);
         const response = result.response;
         const text = response.text();
-        console.log(response);
         res.json({ message: text });
     } catch (error) {
         console.error(error);
         res.status(500).json({ error: "Internal server error" });
     }
-})
+});
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+    console.log(`Example app listening on port ${port}`);
+});
